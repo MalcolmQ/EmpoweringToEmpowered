5a0b194507e3e32e6089d4cbf70c51032f8ef612
MalcolmQ <55450029+MalcolmQ@users.noreply.github.com>
2026-06-21 12:44:59 -0400

    Refactor sidenav functions and remove menu toggle

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const menuToggle = document.getElementById("menuToggle");
-const navLinks = document.getElementById("navLinks");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
-
-
 //show Side Navigation bar
  function showSidenav() {
       const sidenav = document.querySelector('.sidenav');
